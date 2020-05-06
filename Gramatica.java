fc97c87057c9721d9b86540112cda8a288fb870f
Xian-E <19002073@galileo.edu>
2020-05-06 10:38:40 -0600

    BASE

--- Gramatica.java
+++ Gramatica.java
@@ -1,6 +1,33 @@
 public class Gramatica{
 
+	public void AFD(){
+		
+	}
+
+	public void AFN(){
+
+	}
+
+	public void CHECK(){
+		
+	}
+
+
 	public static void main(String[] args) throws Exception{
-		//Su codigo aqui
+		String gramatica = args[0];
+		String bandera = args[1];
+		String salida = artgs[2];
+		boolean n1 = bandera.equals("-afn");
+		boolean n2 = bandera.equals("-afd");
+		boolean n3 = bandera.equals("-check");
+		if(n1){
+
+		}else if(n2){
+
+		}else if(n3){
+			String cuerdas = args[3];
+		}else{
+			System.out.println("NO SEAN PENDEJOS PS");
+		}
 	}
 }
